8b99d67f65e439012a8ed3d44dd8214942cf368c
Juanma Alvarez <juanma.alvarez@cabify.com>
2021-06-25 19:40:48 +0200

    Chapter 10
    Concurrency in Go

---
+++ ch10/done_channel.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+	f := func(s string) []string {
+		return []string{s}
+	}
+	searchers := []func(string) []string{f}
+	searchData("", searchers)
+}
+func searchData(s string, searchers []func(string) []string) []string {
+	done := make(chan struct{})
+	result := make(chan []string)
+	for _, searcher := range searchers {
+		go func(searcher func(string) []string) {
+			select {
+			case result <- searcher(s):
+			case <-done:
+			}
+		}(searcher)
+	}
+	r := <-result
+	close(done)
+	return r
+}
